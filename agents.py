f36fea1e46bff72c7edd2452e9f19771f3fdc192
Nicholas Leo Martin <nickleomartin@gmail.com>
2018-06-15 22:13:40 +0200

    Reorganised repo: separated policy networks and agents (which call policy networks)

--- agents.py
+++ agents.py
@@ -12,79 +12,95 @@ from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 
 
-class BasePolicyModel(object):
-	""" Parent model with save and load methods """
-	def __init__(self):
-		self._model = None
-		self._config_dict = None
-		self.load_config()
 
-	def load_config(self):
-		assert self._config_dict is not None. "Please pass in the config dictionary"
-		self._max_message_length = self.config_dict['max_message_length']
-		self._alphabet = self.config_dict['alphabet']
-		self._alphabet_size = self.config_dict['alphabet_size']
-		self._speaker_lr = self.config_dict['speaker_lr']
-		self._speaker_dim = self.config_dict['speaker_dim']
-		self._listener_lr = self.config_dict['listener_lr']
-		self._listener_dim = self.config_dict['listener_dim']
-		self._training_epoch = self.config_dict['training_epoch']
-		self._batch_size = self.config_dict['batch_size']
-		self._n_classes = self.config_dict['n_distractors'] + 1
-
-	def save(self, weights_file, params_file):
-		pass
-
-	def save_weights(self, file_path):
-		pass
-
-	def save_params(self, file_path):
-		pass
-
-	@classmethod
-	def load_params(cls, file_path):
-		pass
-
-	@classmethod
-	def load(cls, weights_file, params_file):
-		pass
+# class BaseAgent(object):
+# 	""" Parent agent """
+# 	def __init__(self):
+# 		pass
 
 
 
+class RandomAgent(object):
+	"""
+	Agent that randomly chooses messages and targets as a baseline
 
+	Example
+	-------
+	from config import config_dict
+	from data_generator import generate_dummy_data
+	from networks import RandomAgent
+	
+	## Get training data
+	train_data = generate_dummy_data()
 
-class SpeakerPolicy(BasePolicyModel):
-	""" Parent speaker policy model """
-	def __inti__(self, model, config_dict):
-		super(BasePolicyModel).__init__()
-		self._model = model
-		self._config_dict = config_dict
-		
-	def sample_speaker_policy(self, target_input):
-		""" Stochastically sample message of length self.max_message_length from speaker policy """ 
-		x = target_input.reshape([1, self.speaker_dim, 1])
-		probs = self._model.predict(x, batch_size=1)
-		normalized_probs = probs/np.sum(probs)
+	## Initialize and train agent
+	ra = RandomAgent(config_dict)
+	ra.fit(train_data)
 
-		message = ""
-		message_probs = []
-		for i in range(self.max_message_length):
-			sampled_symbol = np.random.choice(self.alphabet_size, 1, p=normalized_probs[0])[0]
-			message += str(sampled_symbol) + "#"
-			message_probs.append(normalized_probs[0][sampled_symbol])
-		
-		## TODO: Also return sum[log prob () mi | target input and weights)]??
-		return message, message_probs
+	"""
+	def __init__(self, config_dict, save_training_stats=True, save_testing_stats=True):
+		self.config_dict = config_dict
+		self.save_training_stats = save_training_stats
+		self.save_testing_stats = save_testing_stats
+		self.training_stats = None
+		self.testing_stats = None
+		self.initialize_parameters()
 
-	def train_on_batch(self, reward):
-		pass
+	def initialize_parameters(self):
+		""" Assign config parameters to local vars """
+		self.max_message_length = self.config_dict['max_message_length']
+		self.alphabet = self.config_dict['alphabet']
 
-	def infer_speaker_policy(self):
-		pass
+	def speaker_policy(self,target_input,max_message_length=2):
+		""" Randomly generate a message """
+		return "".join([np.random.choice(self.alphabet) for i in range(self.max_message_length)])
 
+	def listener_policy(self,message,candidates):
+		""" Randomly choose a target """
+		return np.random.randint(len(candidates))
 
+	def calculate_reward(self, chosen_target_idx, target_candidate_idx):
+		""" Determine reward given indices """
+		if chosen_target_idx==target_candidate_idx:
+			return 1
+		else:
+			return 0
 
+	def fit(self, train_data):
+		""" Random Sampling of messages and candidates for training"""
+		self.training_stats = []
+		total_reward = 0
+		for target_input, candidates, target_candidate_idx in train_data:
+			message = self.speaker_policy(target_input)
+			chosen_target_idx = self.listener_policy(message,candidates)
+			reward = self.calculate_reward(chosen_target_idx,target_candidate_idx)
+			total_reward += reward
 
+			if self.save_training_stats:
+				self.training_stats.append({
+											"reward": reward,
+											"input": target_input,
+											"message": message,
+											"chosen_target": candidates[chosen_target_idx]
+											})
+
+	def predict(self,test_data):
+		""" Random Sampling of messages and candidates for testing"""
+		self.testing_stats = []
+		total_reward = 0
+		for target_input, candidates, target_candidate_idx in test_data:
+			message = self.speaker_policy(target_input)
+			chosen_target_idx = self.listener_policy(message,candidates)
+			reward = self.calculate_reward(chosen_target_idx,target_candidate_idx)
+			total_reward += reward
+
+			if self.save_training_stats:
+				self.testing_stats.append({
+											"reward": reward,
+											"input": target_input,
+											"message": message,
+											"chosen_target": candidates[chosen_target_idx]
+											})
 
 
 
